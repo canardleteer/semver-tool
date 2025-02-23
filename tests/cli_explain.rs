b143231f22a43fe1a0b525a32e40654c52ca7e0f
canardleteer <eris.has.a.dad+github@gmail.com>
2025-02-23 10:42:48 -0800

    feat: lightweight explain and sort cli tests

---
+++ tests/cli_explain.rs
@@ -0,0 +1,25 @@
+use assert_cmd::Command;
+
+#[test]
+fn compare_boring_cases() {
+    // Error status with no args.
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.assert().append_context("explain", "no args").failure();
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd.arg("--help").assert();
+    assert.append_context("explain", "help").success();
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd.arg("explain").arg("a.b.c").assert();
+    assert
+        .append_context("explain", "1 bad semver args")
+        .failure();
+}
+
+#[test]
+fn compare_basic_cases() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd.arg("explain").arg("0.1.2-rc.0.a.1.b+a.0.b.1").assert();
+    assert.append_context("explain", "help").success();
+}
