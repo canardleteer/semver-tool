05b061b1142483fa534a76f90842866293727436
canardleteer <eris.has.a.dad+github@gmail.com>
2025-02-23 10:29:35 -0800

    feat: add compare --semantic-exit-status

--- tests/cli_basics.rs
+++ tests/cli_basics.rs
@@ -5,4 +5,8 @@ fn does_it_exist() {
     let cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"));
     let output = cmd.unwrap();
     println!("{:?}", output);
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd.arg("--help").assert();
+    assert.append_context("basic", "help").success();
 }
--- tests/cli_compare.rs
+++ tests/cli_compare.rs
@@ -4,10 +4,13 @@ use assert_cmd::Command;
 fn compare_boring_cases() {
     // Error status with no args.
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    cmd.assert().append_context("compare", "no args").failure();
+    cmd.arg("compare")
+        .assert()
+        .append_context("compare", "no args")
+        .failure();
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    let assert = cmd.arg("--help").assert();
+    let assert = cmd.arg("compare").arg("--help").assert();
     assert.append_context("compare", "help").success();
 
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
@@ -23,6 +26,8 @@ fn compare_boring_cases() {
         .failure();
 }
 
+/// NOTE(canardleteer): Since these codes are considered unstable for now,
+///                     be prepared to make changes in here.
 #[test]
 fn compare_basic_cases() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
@@ -32,6 +37,7 @@ fn compare_basic_cases() {
         .append_context("compare", "no exit code reporting")
         .success();
 
+    // Should be (sem: Equal, lex: Equal) aka Success
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     let assert = cmd
         .arg("compare")
@@ -39,11 +45,11 @@ fn compare_basic_cases() {
         .arg("1.2.3")
         .arg("1.2.3")
         .assert();
-    // Should be (sem: Equal, lex: Equal) aka Success
     assert
         .append_context("compare", "exit code reporting")
         .success();
 
+    // Should be (sem: Less, lex: Less) aka 100
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     let assert = cmd
         .arg("compare")
@@ -51,11 +57,11 @@ fn compare_basic_cases() {
         .arg("1.2.3")
         .arg("4.5.6")
         .assert();
-    // Should be (sem: Less, lex: Less) aka 100
     assert
         .append_context("compare", "exit code reporting")
         .code(100);
 
+    // Should be (sem: Greater, lex: Greater) aka 122
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     let assert = cmd
         .arg("compare")
@@ -63,11 +69,11 @@ fn compare_basic_cases() {
         .arg("4.5.6")
         .arg("1.2.3")
         .assert();
-    // Should be (sem: Greater, lex: Greater) aka 122
     assert
         .append_context("compare", "exit code reporting")
         .code(122);
 
+    // Should be (sem: Equal, lex: Greater) aka 112
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     let assert = cmd
         .arg("compare")
@@ -75,11 +81,11 @@ fn compare_basic_cases() {
         .arg("1.2.3+1")
         .arg("1.2.3+0")
         .assert();
-    // Should be (sem: Equal, lex: Greater) aka 112
     assert
         .append_context("compare", "exit code reporting")
         .code(112);
 
+    // Should be (sem: Equal, lex: Less) aka 110
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     let assert = cmd
         .arg("compare")
@@ -87,8 +93,54 @@ fn compare_basic_cases() {
         .arg("1.2.3+0")
         .arg("1.2.3+1")
         .assert();
-    // Should be (sem: Equal, lex: Less) aka 110
     assert
         .append_context("compare", "exit code reporting")
         .code(110);
+
+    // Should be (sem: Equal, lex: Less) aka 110, but overridden by -s
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd
+        .arg("compare")
+        .arg("-e")
+        .arg("-s")
+        .arg("1.2.3+0")
+        .arg("1.2.3+1")
+        .assert();
+    assert
+        .append_context(
+            "compare",
+            "exit code reporting + semantic equvalence passing",
+        )
+        .success();
+
+    // Should be (sem: Less, lex: Less) aka 100, and where -s has no impact
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd
+        .arg("compare")
+        .arg("-e")
+        .arg("-s")
+        .arg("1.2.2")
+        .arg("1.2.3+1")
+        .assert();
+    assert
+        .append_context(
+            "compare",
+            "exit code reporting + semantic equvalence passing",
+        )
+        .code(100);
+
+    // These don't match, but should pass anyways since -e is not set
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    let assert = cmd
+        .arg("compare")
+        .arg("-s")
+        .arg("1.2.4+0")
+        .arg("1.2.3+1")
+        .assert();
+    assert
+        .append_context(
+            "compare",
+            "semantic equvalence passing without complex exit code reporting",
+        )
+        .success();
 }
