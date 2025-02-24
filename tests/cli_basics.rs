96b237dc4beeaa582393a8ebead940f54719badd
canardleteer <eris.has.a.dad+github@gmail.com>
2025-02-23 22:16:41 -0800

    fix: better testing on cli_basics

--- tests/cli_basics.rs
+++ tests/cli_basics.rs
@@ -2,10 +2,13 @@ use assert_cmd::Command;
 
 #[test]
 fn does_it_exist() {
-    let cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"));
-    let output = cmd.unwrap();
-    println!("{:?}", output);
+    // Fail with no input.
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .assert()
+        .failure();
 
+    // Success with --help.
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     let assert = cmd.arg("--help").assert();
     assert.append_context("basic", "help").success();
