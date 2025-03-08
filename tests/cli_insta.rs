715f4bab2802ae148c4c985eee22fa6164157b2d
canardleteer <eris.has.a.dad+github@gmail.com>
2025-03-08 08:17:10 -0800

    chore: reduce magic subcommands

--- tests/cli_insta.rs
+++ tests/cli_insta.rs
@@ -15,6 +15,9 @@
 use insta_cmd::{assert_cmd_snapshot, get_cargo_bin};
 use std::process::Command;
 
+mod common;
+use common::subcommands::*;
+
 fn cli() -> Command {
     Command::new(get_cargo_bin("sem-tool"))
 }
@@ -29,48 +32,48 @@ fn cli_insta() {
     //
     // NOTE(canardleteer): No `generate` tests yet.
     let insta_targets = vec![
-        vec!["filter-test", ">a.b.c"],
-        vec!["filter-test", ">1", "x.y.z"],
-        vec!["filter-test", "2.0.0", ">1"],
-        vec!["filter-test", ">1", "2.0.0"],
-        vec!["filter-test", ">1", "0.0.1-rc1.br.0+abc"],
-        vec!["sort", "0.1.2-rc0"],
+        vec![COMMAND_FILTER_TEST, ">a.b.c"],
+        vec![COMMAND_FILTER_TEST, ">1", "x.y.z"],
+        vec![COMMAND_FILTER_TEST, "2.0.0", ">1"],
+        vec![COMMAND_FILTER_TEST, ">1", "2.0.0"],
+        vec![COMMAND_FILTER_TEST, ">1", "0.0.1-rc1.br.0+abc"],
+        vec![COMMAND_SORT, "0.1.2-rc0"],
         vec![
-            "sort",
+            COMMAND_SORT,
             "--lexical-sorting",
             "--fail-if-potentially-ambiguous",
             "0.1.2+bm0",
             "0.1.2+bm1",
         ],
-        vec!["sort", "--lexical-sorting", "0.1.2+bm0", "0.1.2+bm1"],
-        vec!["sort", "-r", "0.1.2-rc0", "0.1.2-rc1"],
-        vec!["sort", "--flatten", "0.1.2-rc0", "0.1.2-rc1"],
-        vec!["sort", "--lexical-sorting", "0.1.2-rc0", "0.1.2-rc1"],
+        vec![COMMAND_SORT, "--lexical-sorting", "0.1.2+bm0", "0.1.2+bm1"],
+        vec![COMMAND_SORT, "-r", "0.1.2-rc0", "0.1.2-rc1"],
+        vec![COMMAND_SORT, "--flatten", "0.1.2-rc0", "0.1.2-rc1"],
+        vec![COMMAND_SORT, "--lexical-sorting", "0.1.2-rc0", "0.1.2-rc1"],
         vec![
-            "sort",
+            COMMAND_SORT,
             "--lexical-sorting",
             "--flatten",
             "0.1.2-rc0",
             "0.1.2-rc1",
         ],
-        vec!["sort", "-f", ">1", "0.1.2-rc0", "0.1.2-rc1"],
-        vec!["sort", "-f", ">0", "0.1.2-rc0", "0.1.2-rc1"],
-        vec!["sort", "-f", ">a", "0.1.2-rc0", "0.1.2-rc1"],
-        vec!["validate", "a.b.c"],
-        vec!["validate", "0.1.2-rc.0.a.1.b+a.0.b.1"],
-        vec!["validate", "-s", "18446744073709551616.0.0"],
-        vec!["validate", "18446744073709551616.0.0"],
-        vec!["explain", "a.b.c"],
-        vec!["explain", "0.1.2-rc.0.a.1.b+a.0.b.1"],
-        vec!["compare", "1.2.3", "4.5.6"],
-        vec!["compare", "-e", "1.2.3", "1.2.3"],
-        vec!["compare", "-e", "1.2.3", "4.5.6"],
-        vec!["compare", "-e", "4.5.6", "1.2.3"],
-        vec!["compare", "-e", "1.2.3+1", "1.2.3+0"],
-        vec!["compare", "-e", "1.2.3+0", "1.2.3+1"],
-        vec!["compare", "-e", "-s", "1.2.3+0", "1.2.3+1"],
-        vec!["compare", "-e", "-s", "1.2.2", "1.2.3+1"],
-        vec!["compare", "-s", "1.2.4+0", "1.2.3+1"],
+        vec![COMMAND_SORT, "-f", ">1", "0.1.2-rc0", "0.1.2-rc1"],
+        vec![COMMAND_SORT, "-f", ">0", "0.1.2-rc0", "0.1.2-rc1"],
+        vec![COMMAND_SORT, "-f", ">a", "0.1.2-rc0", "0.1.2-rc1"],
+        vec![COMMAND_VALIDATE, "a.b.c"],
+        vec![COMMAND_VALIDATE, "0.1.2-rc.0.a.1.b+a.0.b.1"],
+        vec![COMMAND_VALIDATE, "-s", "18446744073709551616.0.0"],
+        vec![COMMAND_VALIDATE, "18446744073709551616.0.0"],
+        vec![COMMAND_EXPLAIN, "a.b.c"],
+        vec![COMMAND_EXPLAIN, "0.1.2-rc.0.a.1.b+a.0.b.1"],
+        vec![COMMAND_COMPARE, "1.2.3", "4.5.6"],
+        vec![COMMAND_COMPARE, "-e", "1.2.3", "1.2.3"],
+        vec![COMMAND_COMPARE, "-e", "1.2.3", "4.5.6"],
+        vec![COMMAND_COMPARE, "-e", "4.5.6", "1.2.3"],
+        vec![COMMAND_COMPARE, "-e", "1.2.3+1", "1.2.3+0"],
+        vec![COMMAND_COMPARE, "-e", "1.2.3+0", "1.2.3+1"],
+        vec![COMMAND_COMPARE, "-e", "-s", "1.2.3+0", "1.2.3+1"],
+        vec![COMMAND_COMPARE, "-e", "-s", "1.2.2", "1.2.3+1"],
+        vec![COMMAND_COMPARE, "-s", "1.2.4+0", "1.2.3+1"],
         // NOTE(canardleteer): For now, the `generate` command is omitted.
     ];
 
